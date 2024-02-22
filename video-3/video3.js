09d382369e673ee21860914322d6dc724c27d475
ruby pham <rubypham955@gmail.com>
2024-02-22 13:48:42 -0500

    chapter2-video3

---
+++ video-3/video3.js
@@ -0,0 +1,7 @@
+// dung dau ``thi kieu ben trong du lieu co the dung tat ca loai
+
+console.log("Hello world from Javascript-video3");
+let x = 5,
+  y = 6,
+  z = 7;
+console.log("test string", "x=", x, "y=", y, "z=", z);
